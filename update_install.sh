b5d47fe6cbb4496f933ac6890d93ef1f0802dee1
Christian Stauffer <37304790+ChrIgiSta@users.noreply.github.com>
2018-11-28 15:40:55 +0100

    Update update_install.sh

--- update_install.sh
+++ update_install.sh
@@ -4,4 +4,5 @@ sudo systemctl stop wpantund.service
 ./bootstrap.sh
 ./configure --prefix=/usr --sysconfdir=/etc --disable-ncp-dummy --enable-static-link-ncp-plugin=spinel
 make && sudo make install
+sudo sed -i '/Config:NCP:SocketPath "\/dev/i Config:NCP:SocketPath "/dev/ncpRadio"' /etc/wpantund.conf
 sudo systemctl start wpantund.service
